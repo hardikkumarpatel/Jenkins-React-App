276c7ed2f637f847b7faca5beeab97fd6f521fb1
Hardik Patel <67120721+hardikkumarpatel@users.noreply.github.com>
2025-03-17 12:58:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <strong>
+          Hello, Hardik Patel
+        </strong>
+       <div>
+         Here is the working example of Jenkins CI/CD
+       </div>
         <a
           className="App-link"
           href="https://reactjs.org"
